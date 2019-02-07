1b2ec76d6ac609946c17fd7d8404d8e11941c45d
Bryan Galvin <bryan@priceflow.com>
2019-02-06 23:20:29 -0800

    add upgrade

--- user_data.sh
+++ user_data.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo apt-get update
+sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install -y postgresql \
                         postgresql-contrib \
                         awscli \
